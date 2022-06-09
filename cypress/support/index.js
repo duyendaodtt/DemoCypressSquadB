a2c37789987dd6254709a01bba0e9a9668c26262
hoanghadhpt <33559387+hoanghadhpt@users.noreply.github.com>
2022-06-09 13:45:05 +0700

    Revert "Duyen jun"

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,7 +16,6 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import './fixtures'
-require("cypress-xpath");
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
